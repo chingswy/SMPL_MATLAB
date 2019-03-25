b57df1492680fbda75ffe2f4505e3bf1ba396e81
shuaiqing <shuaiqing@sensetime.com>
2019-03-25 22:15:15 +0800

    format

--- utils/so3exp.m
+++ utils/so3exp.m
@@ -1,45 +1,50 @@
 % Emanuele Ruffaldi 2017 @ SSSA
-function R = so3exp(omega)
+function [R, jacobian] = so3exp(omega)
 
-theta = norm(omega);
-if theta < 1e-12
-    % Original
-    %     A = 1;
-    %     B = 0.5;
-    %     C = 1/6;
-    %     S = zeros(3);
-    %     R = eye(3) + A*S + B*S^2;
-    %     V = eye(3) + B*S + C*S^2;
-    
+    needJacobian = (nargout > 1);
+
+    theta = norm(omega);
+
+    if theta < 1e-12
+        % Original
+        %     A = 1;
+        %     B = 0.5;
+        %     C = 1/6;
+        %     S = zeros(3);
+        %     R = eye(3) + A*S + B*S^2;
+        %     V = eye(3) + B*S + C*S^2;
+
         N = 10;
         R = eye(3);
         xM = eye(3);
         cmPhi = skew(omega);
+
         for n = 1:N
             xM = xM * (cmPhi / n);
             R = R + xM;
         end
-        
+
         % Project the resulting rotation matrix back onto SO(3)
-        R = R / sqrtm( R'*R ) ;
-    
-else
-    %Original
-    A = sin(theta)/theta;
-    B = (1-cos(theta))/(theta^2);
-    C = (theta-sin(theta))/(theta^3);
-    S = skew(omega);
-    R = eye(3) + A*S + B*S^2;
-    %V = eye(3) + B*S + C*S^2;
-    
-    %Barfoot
-    if 0==1    
-        axis = omega/theta;
-        cp = cos(theta);
-        sp = sin(theta);
-        sa = skew(axis);
-        
-        R = cp*eye(3) + (1-cp)*axis*(axis') + sp*sa;
+        R = R / sqrtm(R' * R);
+
+    else
+        %Original
+        A = sin(theta) / theta;
+        B = (1 - cos(theta)) / (theta^2);
+        C = (theta - sin(theta)) / (theta^3);
+        S = skew(omega);
+        R = eye(3) + A * S + B * S^2;
+        %V = eye(3) + B*S + C*S^2;
+
+        %Barfoot
+        if 0 == 1
+            axis = omega / theta;
+            cp = cos(theta);
+            sp = sin(theta);
+            sa = skew(axis);
+
+            R = cp * eye(3) + (1 - cp) * axis * (axis') + sp * sa;
+        end
+
+        assert(abs(det(R) - 1) < 1e-5, 'unitary');
     end
-        assert(abs(det(R)-1) < 1e-5,'unitary');
-end
--- utils/so3log.m
+++ utils/so3log.m
@@ -1,19 +1,20 @@
 function omega = so3log(R)
 
-    theta = acos((trace(R)-1)/2); %acos(max(-1,min((trace(R)-1)/2,1)));
+    theta = acos((trace(R) - 1) / 2); %acos(max(-1,min((trace(R)-1)/2,1)));
+
     if isreal(theta) == 0
-        R = R/abs(det(R));
-        theta =  acos((trace(R)-1)/2);
+        R = R / abs(det(R));
+        theta = acos((trace(R) - 1) / 2);
     end
 
     if abs(theta) < 1e-10
         B = 0.5;
-        SO = (1/(2))*(R-R');  % =skew(omega)
+        SO = (1 / (2)) * (R - R'); % =skew(omega)
         iV = eye(3); % use directly skew of omega
     else
-        A = sin(theta)/theta;
-        B = (1-cos(theta))/(theta*theta);
-        SO = (1/(2*A))*(R-R');  % =skew(omega)
+        A = sin(theta) / theta;
+        B = (1 - cos(theta)) / (theta * theta);
+        SO = (1 / (2 * A)) * (R - R'); % =skew(omega)
         %??
         % syms x real
         % A = sin(x)/x
@@ -21,10 +22,8 @@ function omega = so3log(R)
         % Q=1/(x*x)*(1 - A/2/B)
         % taylor(Q,x,0)
         %       x^4/30240 + x^2/720 + 1/12
-        Q= 1/(theta^2)*(1 - A/2/B);
-        iV = eye(3) - 1/2*SO + Q*SO^2; % use directly skew of omega
+        Q = 1 / (theta^2) * (1 - A / 2 / B);
+        iV = eye(3) - 1/2 * SO + Q * SO^2; % use directly skew of omega
     end
 
-    omega = [SO(3,2) SO(1,3) SO(2,1)];
-
-    
+    omega = [SO(3, 2) SO(1, 3) SO(2, 1)];
